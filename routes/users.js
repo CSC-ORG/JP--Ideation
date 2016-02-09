8140d1535dcbedfcb4d4a3e020381a7563ba132f
sunnykarira <sunnykarira@outlook.com>
2016-02-09 17:12:22 +0530

    Add GET /login and GET /register route

--- routes/users.js
+++ routes/users.js
@@ -2,8 +2,21 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
+/*router.get('/', function(req, res, next) {
   res.send('respond with a resource');
+});*/
+
+router.get('/register', function(req, res, next){
+	res.render('register', {
+		'title': 'Register'
+	});
 });
+
+router.get('/login', function(req, res, next){
+	res.render('login', {
+		'title': 'Login'
+	});
+});
+
 
 module.exports = router;

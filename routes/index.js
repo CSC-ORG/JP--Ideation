6eaefed7cf2009ee9cdf3e6edb541bedece2c7e2
sunnykarira <sunnykarira@outlook.com>
2016-02-22 18:10:58 +0530

    Add wow.js animate.css and font-awesome.css

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,21 @@
 var express = require('express');
 var router = express.Router();
 
+function ensureAuthenticated(req, res, next){
+	//Passport Authentication API
+	if(req.isAuthenticated()){
+		return next();
+	}	
+	res.redirect('/users/login');
+}
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', ensureAuthenticated, function(req, res, next) {
+  res.render('index', { title: 'Members Area' });
+});
+
+router.get('/home', function(req, res, next) {
+  res.render('externalindex', { title: 'Home' });
 });
 
 module.exports = router;
